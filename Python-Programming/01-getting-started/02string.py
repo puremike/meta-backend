163a604ba15cdd513f765368a13b88def7670fe8
Michael Egbinola <michaelmarketing25@gmail.com>
2023-07-26 00:30:34 -0700

    learnt more about the string methods and wrote some functions to perform specific tasks

--- Python-Programming/01-getting-started/02string.py
+++ Python-Programming/01-getting-started/02string.py
@@ -1,10 +1,6 @@
 a = "Mike"
 print(len(a))
 
-#print a string in two lines
-b = "Hello, I'm learning Python. I intend to use \n python for Backend Development and Cloud Engineering"
-print(b)
-
 #concatenation of string
 c= "Hello"
 d = "Michael"
@@ -12,9 +8,39 @@ print(c + " " + d + "!")
 
 #indexing a string
 print(a[1-3])
+print(a[:4])
 print(d[4])
 
 #convert a number to a string
 e = 55;
 f = str(e) #type casting
 print(type(f) == str) #checking if the condition is true or false (string or no-string)
+
+#reverse a string
+nString = "enola"
+print("reversed string:", nString[::-1])
+
+#convert a string to upper and lowercase
+print("lower:", nString.lower())
+print("upper:", nString.upper())
+
+#capitalize a string
+print("capitalize string:", nString.capitalize())
+
+#split a string and return a list of it
+mString = "Welcome to lagos, Nigeria"
+print(mString.split())
+
+#isLower(), isUpper(), isCapitalize(), is..........
+
+# find() -Searches the string for a specified value and returns the position of where it was found
+print("find:", mString.find("Nigeria"))
+
+#strip() - trim a string
+sString = "    Welcome boys!   "
+print("trimmed string:", sString.strip())
+
+#join() to join an iterable to a string
+pString = ["michael12", "gmail.com"]
+print("@".join(pString))
+
---
+++ Python-Programming/01-getting-started/10exercises.py
@@ -0,0 +1,52 @@
+#funtion to find the maximum number
+num = [2,6,8,2,5,9]
+num1 = [10, 19, 45, 78, 23, 56, 77, 97]
+def maxNum(*args):
+    max = 0
+    for n in args:
+        n > max
+    print("Maximum Number:", n)
+maxNum(*num)
+maxNum(*num1)
+
+#min() and max() method
+# print(max(num))
+# print(min(num))
+
+# #funtion to find the minumum number
+def minNum(*args):
+    mini = min(args)
+    print("Minimum Number:", mini)
+minNum(*num)
+minNum(*num1)
+
+#Write a Python function to multiply all the numbers in a list.
+print("\n Multiplication of numbers:")
+def mult(*args):
+    m = 1
+    for n in args:
+        m *= n
+    return m
+print(mult(*num))
+print(mult(*num1))
+
+# Write a Python function to calculate the factorial of a number (a non-negative integer). The function accepts the number as an argument.
+def factorial(num):
+    count = 1
+    for n in range(1,num+1):
+        count *= n
+    print("Factorial of {0} = {1}".format(num, count))    
+factorial(15)
+
+#a function to check is a string is palindrome
+def checkPalindrome(str):
+    reversedString = str[::-1].lower()
+    if(reversedString == str.lower()):
+        return True
+    else:
+        return False
+print(checkPalindrome("AzA"))
+print(checkPalindrome("AZA"))
+print(checkPalindrome("aZA"))
+print(checkPalindrome("madam"))
+print(checkPalindrome("enoLA"))
